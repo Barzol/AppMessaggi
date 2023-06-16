9c54d9df736602335378a4ea1dee481f6a9b163f
Favuzzi23 <antoniofavuzzi01@gmail.com>
2023-06-16 19:09:09 +0200

    Container e sidebarChat

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import './App.css'
+import ChatBox from "./components/ChatBox.jsx";
+import ChatContainer from "./components/ChatContainer.jsx";
+import Message from "./components/Message.jsx";
+import Sidebar from "./components/Sidebar.jsx";
+
+export default function App(){
+
+    return(
+        <>
+        <div className="app">
+            <div className="app_body">
+                
+            </div>
+        </div>
+    </>
+    )
+}
