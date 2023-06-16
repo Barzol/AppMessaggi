5c9c187fc69ceedc1753dd087c704aa401059851
Favuzzi23 <antoniofavuzzi01@gmail.com>
2023-06-16 19:49:36 +0200

    Merge remote-tracking branch 'origin/main' into default

--- client/src/components/ChatBox.jsx
+++ client/src/components/ChatBox.jsx
@@ -4,25 +4,37 @@ import Message from "./Message";
 
 export default function ChatBox(data){
     return(
-        // mandati = messaggi sulla destra
-        // ricevuti = messaggi sulla sinistra
-        <>
-            <div id='mandati'>
-                <ul>
-                    <li> {data.messages.map((c,i) =>
-                        <Message user={c.username} content={c.content}/>
-                        )}
-                    </li>
-                </ul>
+        <div className="chat">
+            <div className="chat_header">
+                <h2>header</h2>
             </div>
-            <div id='ricevuti'>
-                <ul>
-                    <li> {data.messages.map((c,i) =>
-                        <Message user={c.username} content={c.content}/>
-                    )}
-                    </li>
-                </ul>
+            <div className="chat_body">
+                <h2>body</h2>
             </div>
-        </>
+        </div>
     )
 }
+
+
+
+/*
+// mandati = messaggi sulla destra
+// ricevuti = messaggi sulla sinistra
+<>
+    <div id='mandati'>
+        <ul>
+            <li> {data.messages.map((c,i) =>
+                <Message user={c.username} content={c.content}/>
+            )}
+            </li>
+        </ul>
+    </div>
+    <div id='ricevuti'>
+        <ul>
+            <li> {data.messages.map((c,i) =>
+                <Message user={c.username} content={c.content}/>
+            )}
+            </li>
+        </ul>
+    </div>
+</> */
